e79b11ab9cbb9093eb9dc404724dedb466b7c736
anshu0x <anshusharma6327@gmail.com>
2022-09-08 16:01:07 +0530

    bind function

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ let a = { name: "anshu" };
 function printName(c) {
   console.log("my name is " + this.name + " my class is " + c);
 }
-//  the second argument will be paramter of that function 
-printName.call(a,"Bca")
+//  the second argument will be paramter of that function
+printName.call(a, "Bca");
 
-// apply method 
+// apply method
 
 // the only differnce is in aplly we have to pass the fucntion arguement inside the array
 
@@ -17,7 +17,18 @@ function printName2(c) {
   console.log("my name is " + this.name + " my class is " + c);
 }
 
-printName2.apply(b,["Bca"])
+printName2.apply(b, ["Bca"]);
 
-//  bind method 
+//  bind method
 
+// call and apply are invoked immediate but bind provide us reusable fucntion that can be called later
+
+let c = { name: "sharma" };
+
+function printName3() {
+  console.log("My name is anshu", this.name);
+}
+
+const d = printName3.bind(c);
+
+d();
