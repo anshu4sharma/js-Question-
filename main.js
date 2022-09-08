171befd7f0b4ea50178df190b02299b3ba3f6560
anshu0x <anshusharma6327@gmail.com>
2022-09-08 15:53:18 +0530

    call apply

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-
+//call Method
 let a = { name: "anshu" };
 
-function printName() {
-  console.log("my name is " + this.name);
+function printName(c) {
+  console.log("my name is " + this.name + " my class is " + c);
+}
+//  the second argument will be paramter of that function 
+printName.call(a,"Bca")
+
+// apply method 
+
+// the only differnce is in aplly we have to pass the fucntion arguement inside the array
+
+let b = { name: "anshu" };
+
+function printName2(c) {
+  console.log("my name is " + this.name + " my class is " + c);
 }
 
-printName.call(a)
+printName2.apply(b,["Bca"])
+
+//  bind method 
+
